add7eca2d3d60810b6acf9fbbe343f1720cc35fc
zhumagalibota <zhumagalibota@outlook.com>
2024-02-13 20:57:35 +0600

    to do list done

--- lab3/task2/1.js
+++ lab3/task2/1.js
@@ -1,12 +1,12 @@
 const input = document.getElementById("input");
 const tasks = document.getElementById("tasks");
-function newTask(){
-    if(input.value === ''){
+
+function newTask() {
+    if (input.value === '') {
         alert("No input");
-    }
-    else{
+    } else {
         let li = document.createElement("li");
-        li.innerHTML = input.value;
+        li.textContent = input.value; 
         tasks.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -15,21 +15,26 @@ function newTask(){
     input.value = "";
     saveData();
 }
-tasks.addEventListener("Click", function(e){
-    if(e.targer.tagName === "LI"){
+
+tasks.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") { 
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", tasks.innerHTML);
 }
-function showTask(){
-    tasks.innerHTML = localStorage.getItem("data");
+
+function showTask() {
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        tasks.innerHTML = savedData;
+    }
 }
+
 showTask();
