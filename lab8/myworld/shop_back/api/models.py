7878a8f896dc450d4c7c02739ec0c418156d8744
zhumagalibota <zhumagalibota@outlook.com>
2024-04-01 20:21:23 +0600

    task 2

--- lab8/myworld/shop_back/api/models.py
+++ lab8/myworld/shop_back/api/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     count = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=100)
