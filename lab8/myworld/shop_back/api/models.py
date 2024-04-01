0f59bdb98a2ada7444816bcf0a1409ae8ce25cc5
zhumagalibota <zhumagalibota@outlook.com>
2024-04-01 19:43:20 +0600

    .

---
+++ lab8/myworld/shop_back/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
