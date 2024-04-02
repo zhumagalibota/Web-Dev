06adbe6a7e60b5a6674fda85c5348cabf103ebe7
zhumagalibota <zhumagalibota@outlook.com>
2024-04-02 20:14:01 +0600

    .

--- lab8/myworld/shop_back/api/views.py
+++ lab8/myworld/shop_back/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class ProductApiView(APIView):
+    def get(self, request):
+        allProducts = Product.objects.all().values()
+        return Response({"Message":"List of Products", "Product list":allProducts})
+
+    def post(self, request):
+        Product.objects.create(name=request.data["name"],
+                               price=request.data["price"],
+                               description=request.data["description"],
+                               count=request.data["count"],
+                               is_active=request.data["is_active"]
+                              )
+        product = Product.objects.all().filter(name=request.data["name"]).values()
+        return Response({"Message":"New product added", "Product":allProducts})
