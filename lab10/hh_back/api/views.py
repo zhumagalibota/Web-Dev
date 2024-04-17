bc0395e9ddb42c239cdc3b01d31c2d77deed4ba1
zhumagalibota <zhumagalibota@outlook.com>
2024-04-17 20:14:22 +0600

    .

---
+++ lab10/hh_back/api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from .models import Company, Vacancy
+from django.core import serializers
+
+# Create your views here.
+def testingCompany(request):
+    companies = Company.objects.all()
+    data = serializers.serialize('json', companies)
+    return HttpResponse(data, content_type='application/json')
+
+def testingVacancy(request):
+    vacancies = Vacancy.objects.all()
+    data = serializers.serialize('json', vacancies)
+    return HttpResponse(data, content_type='application/json')
+
+
+def company_detail(request, company_id):
+    company = get_object_or_404(Company, pk=company_id)
+    data = serializers.serialize('json', [company])
+    return HttpResponse(data, content_type='application/json')
+
+
+def vacancy_detail(request, vacancy_id):
+    vacancy = get_object_or_404(Vacancy, pk=vacancy_id)
+    data = serializers.serialize('json', [vacancy])
+    return HttpResponse(data, content_type='application/json')
+
+
+def vacancy_sorted(request):
+    vacancies = Vacancy.objects.order_by('-salary')  
+    data = serializers.serialize('json', vacancies)
+    return HttpResponse(data, content_type='application/json')
+
+
+def vacancies_by_company(request, company_id):
+    company = Company.objects.get(pk=company_id)
+    vacancies = Vacancy.objects.filter(company=company).values('name', 'description', 'salary')
+    vacancies_data = list(vacancies) 
+    return JsonResponse({'company': company.name, 'vacancies': vacancies_data})
---
+++ lab10/hh_back/hh_back/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for hh_back project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('companies/', views.testingCompany, name='company_list'),
+    path('company/<int:company_id>/', views.company_detail, name='company_detail'),
+    path('vacancies/', views.testingVacancy, name='vacancy_list'),
+    path('vacancy/<int:vacancy_id>/', views.vacancy_detail, name='vacancy_detail'),
+    path('vacancies_sorted/', views.vacancy_sorted, name='vacancy_sorted_by_salary'),
+    path('company/<int:company_id>/vacancies/', views.vacancies_by_company, name='vacancies_by_company'),
+]
